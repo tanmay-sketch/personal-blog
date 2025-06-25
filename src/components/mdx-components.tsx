52bfc4fd3a8ff35e4d65075f409b26ae1456a525
tanmay-sketch <grandhi1@msu.edu>
2025-06-25 22:02:41 +0530

    added custom components

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,5 +1,6 @@
 import * as runtime from "react/jsx-runtime";
 import Image from "next/image";
+import { Callout } from "./callout";
 
 const useMDXComponent = (code: string) => {
     const fn = new Function(code);
@@ -7,7 +8,8 @@ const useMDXComponent = (code: string) => {
 };
 
 const components = {
-    Image
+    Image,
+    Callout
 }
 
 interface MdxProps {

67f31150e303757110dbc75df57ffe7e8cb0d70b
tanmay-sketch <grandhi1@msu.edu>
2025-06-26 16:46:41 +0530

    pagination done

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,26 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-item";
+import { QueryPagination } from "@/components/query-pagination";
 import { sortPosts } from "@/lib/utils";
 
-export default async function BlogPage() {
+const POSTS_PER_PAGE = 5;
+
+interface BlogPageProps {
+    searchParams: Promise<{
+        page?: string;
+    }>;
+}
+
+export default async function BlogPage( { searchParams }: BlogPageProps ) {
+    const params = await searchParams;
+    const currentPage = Number(params?.page) || 1;
     const sortedPosts = sortPosts(posts.filter((post) => post.published));
-    const displayPosts = sortedPosts;
+    const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+    const displayPosts = sortedPosts.slice(
+        POSTS_PER_PAGE * (currentPage - 1),
+        POSTS_PER_PAGE * currentPage
+    );
 
     return (
         <div className="container max-w-4xl px-4 py-6 lg:py-10">
@@ -27,6 +43,7 @@ export default async function BlogPage() {
                     </li>
                 })}
             </ul>) : <p>Nothing to see here yet</p>}
+            <QueryPagination totalPages={totalPages} className=" justify-end mt-4"/>
         </div>
     )
 }
